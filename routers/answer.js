1d2c815650493bf50e2d0ce0dd0eec590e6e540a
Elvin Husubayli <husubayli@Elvins-MacBook-Pro.local>
2022-09-23 11:50:18 +0400

    merge params for router

--- routers/answer.js
+++ routers/answer.js
@@ -2,12 +2,11 @@ const express = require('express')
 
 const router = express.Router({mergeParams: true})
 
+const { getAccessToRoute } = require('../middlewares/authorization/auth')
 
 
-router.get('/', (req, res, next) => {
 
-    return res.send('Answer Route')
-})
+router.get('/', getAccessToRoute, askNewAnswerToQuestion)
 
 
 module.exports = router
